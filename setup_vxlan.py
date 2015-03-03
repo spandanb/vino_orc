c7f457ca366b1759df712af52a6165573f2254a8
spandanb <spandan@savinetwork.ca>
2015-03-03 01:38:32 -0500

    Added template file to setup VXLAN overlay

---
+++ setup_vxlan.py
@@ -0,0 +1,26 @@
+from string import Template
+import sys
+
+#Parameters
+TEMPLATE_FILE = "setup_vxlan_template.sh"
+INSTANCE_FILE = "setup_vxlan.sh"
+VXLAN_IP = "192.168.200.1/24"
+REMOTE_IP = "10.12.1.53"
+
+if __name__ == "__main__":
+    
+    VXLAN_IP = sys.argv[1]
+    REMOTE_IP = sys.argv[2]
+
+    #open the file
+    with open( TEMPLATE_FILE ) as template:
+        #read it
+        src = Template( template.read() )
+        result = src.substitute({'VXLAN_IP': VXLAN_IP, 
+            'REMOTE_IP': REMOTE_IP})
+        #write output
+        with open(INSTANCE_FILE, 'w') as outfile:
+            outfile.write(result)
+            
+
+
