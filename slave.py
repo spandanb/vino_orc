6ec24f56b82470db4ae4322cf7ca5c62d408ab97
spandanb <spandan@savinetwork.ca>
2015-04-30 17:11:01 +0000

    foo

--- slave.py
+++ slave.py
@@ -5,66 +5,9 @@ import socket
 import sys
 import cPickle
 import threading 
+from utils import get_ip_addr
 
-class VinoSlaveL(thread.Thread):
-    """
-    Listens to master for new connections
-    """ 
-    def __init__(self):
-        """
-        Arguments:
-        ip_addr -- IP Address of VINO Master
-        port -- Port VINO Master is listening on
-        """
-        threading.Thread.__init__(self)
-        self.slaves = []
-        #Used to assign increasing (unique) vxlan ip addresses
-        #Value of last octet 
-        self.octet_val = 0 
-        parameters = pika.ConnectionParameters(host='localhost')
-        #This establishes connection to the rabbitmq server
-        #rabbitmq-server is running on same machine as vino-master
-        self.connection = pika.BlockingConnection(parameters)
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue='rpc_queue')
-        self.channel.basic_qos(prefetch_count=1)
-        self.channel.basic_consume(self.on_request, queue='rpc_queue')
-
-    def get_ip_addr(self):
-        """
-        Return IP Address
-        """
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8",80))
-        ip_addr = s.getsockname()[0]
-        s.close()
-        return ip_addr
-    
-    def get_vxlan_ip(self):
-        self.octet_val += 1
-        return "192.168.200.%s" % self.octet_val
-    
-    def on_request(self, ch, method, props, body):
-        client_ip_addr = body
-        print " [.] client IP Address: %s"  % client_ip_addr
-        #TODO: check if ip address is unique
-        self.slaves.append( (client_ip_addr, self.get_vxlan_ip()) )
-        response = cPickle.dumps(self.slaves, cPickle.HIGHEST_PROTOCOL)
-        if True: #len(slaves) == 2: 
-            ch.basic_publish(exchange='',
-                             routing_key=props.reply_to,
-                             properties=pika.BasicProperties(correlation_id = props.correlation_id),
-                             body=response) 
-            ch.basic_ack(delivery_tag = method.delivery_tag)
-
-    def start_consuming(self):
-        print " [x] Awaiting RPC requests"
-        self.channel.start_consuming()
-
-    def run(self):
-        self.start_consuming()
-
-class VinoSlave(threading.Thread):
+class VinoSlave():
     """
     Sends self ip address to ViNO master 
     """
@@ -74,12 +17,12 @@ class VinoSlave(threading.Thread):
         ip_addr -- IP Address of VINO Master
         port -- Port VINO Master is listening on
         """
-        threading.Thread.__init__(self)
         credentials = pika.PlainCredentials('guest', 'guest')
         parameters=pika.ConnectionParameters(ip_addr, port, '/', credentials)
         self.connection = pika.BlockingConnection(parameters)
-
         self.channel = self.connection.channel()
+        self.slaves = {}
+
         #For first contact 
         result = self.channel.queue_declare(exclusive=True)
         self.callback_queue = result.method.queue
@@ -89,7 +32,7 @@ class VinoSlave(threading.Thread):
 
         #For listening for new slaves
         self.channel.exchange_declare(exchange='logs', type='fanout')
-        result = channel.queue_declare(exclusive=True)
+        result = self.channel.queue_declare(exclusive=True)
         queue_name = result.method.queue
 
         self.channel.queue_bind(exchange='logs',
@@ -100,13 +43,18 @@ class VinoSlave(threading.Thread):
                               no_ack=True)
 
     def on_response(self, ch, method, props, body):
+        """
+        On response from initial contact
+        """
         if self.corr_id == props.correlation_id:
-            self.response = body
+            self.response = cPickle.loads(body)
 
-    def communicate(self):
-    """ 
-    Communicates with the ViNO Master
-    """
+    def contact_master(self):
+        """ 
+        Contacts ViNO Master, and sends own
+        IP address
+        response is dictionary of existing slaves 
+        """
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(exchange='',
@@ -115,37 +63,38 @@ class VinoSlave(threading.Thread):
                                          reply_to = self.callback_queue,
                                          correlation_id = self.corr_id,
                                          ),
-                                   body=str(self.get_ip_addr()))
+                                   body=str(get_ip_addr()))
     
         #Loop until self.response is written out
         while self.response is None:
             self.connection.process_data_events()
+        self.slaves = self.response
         return self.response
 
     def callback(self, ch, method, properties, body):
-        print " [x] %r" % (body,)
-
-    def get_ip_addr(self):
-    """
-    Returns IP address  
-    """
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8",80))
-        ip_addr = s.getsockname()[0]
-        s.close()
-        return ip_addr
+        """
+        When new slave broadcast
+        """
+        
+        body = cPickle.loads(body)
+        print "Received: %r" % (body,)
+        self.slaves[body[0]] = body[1]
+        
 
     def start_listening(self):
+        """
+        Listen for new slaves
+        """
         self.channel.start_consuming()
 
 if __name__ == "__main__":
     #IP ADDR of VINO master
     ip_addr = sys.argv[1] if len(sys.argv) > 1 else "10.12.1.53"
     port = 5672 
-    client = VinoSlave(ip_addr, port)
+    node = VinoSlave(ip_addr, port)
     print " [x] Requesting server IP Address"
-    response = cPickle.loads(client.communicate())
+    response = node.contact_master()
     print " [.] Got %r" % (response,)
 
     print "Now listening for new slaves"
-    client.start_listening()
+    node.start_listening()
--- vino_master.py
+++ vino_master.py
@@ -1,72 +1,20 @@
 #!/usr/bin/env python
 
 import pika
-import socket
 import cPickle
 import threading
 
 #TODO: race conditions if 2 slaves join at the same time?
-#TODO: handle clients die 
+#TODO: handle conditions like clients die 
 
-class VinoMasterW(threading.Thread):
-    """
-    Push information about new slaves to 
-    existing slaves
-    """
-    def __init__(self):
-        threading.Thread.__init__(self)
-        parameters = pika.ConnectionParameters(host='localhost')
-        self.connection = pika.BlockingConnection(parameters)
-
-        self.channel = self.connection.channel()
-
-        result = self.channel.queue_declare(exclusive=True)
-        self.callback_queue = result.method.queue
-
-        self.channel.basic_consume(self.on_response, no_ack=True,
-                                   queue=self.callback_queue)
-
-    def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = body
-
-    def communicate(self):
-    """ 
-    Communicates with the ViNO Master
-    """
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(exchange='',
-                                   routing_key='rpc_queue',
-                                   properties=pika.BasicProperties(
-                                         reply_to = self.callback_queue,
-                                         correlation_id = self.corr_id,
-                                         ),
-                                   body=str(self.get_ip_addr()))
-    
-        #Loop until self.response is written out
-        while self.response is None:
-            self.connection.process_data_events()
-        return self.response
-
-    def get_ip_addr(self):
-    """
-    Returns IP address  
-    """
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8",80))
-        ip_addr = s.getsockname()[0]
-        s.close()
-        return ip_addr
-
-class VinoMasterL(threading.Thread):
+class VinoMaster(object):
     """ VINO Master- listens for new slaves and responds, 
     i.e.pushes information about existing slaves to this slave
     This is based on the RPC design pattern
+    Also broadcasts info about new slaves to old slaves
     """
     def __init__(self):
-        threading.Thread.__init__(self)
-        self.slaves = []
+        self.slaves = {}
         #Used to assign increasing (unique) vxlan ip addresses
         #Value of last octet 
         self.octet_val = 0 
@@ -79,63 +27,62 @@ class VinoMasterL(threading.Thread):
         #for new slave 
         self.channel.queue_declare(queue='rpc_queue')
         self.channel.basic_qos(prefetch_count=1)
-        self.channel.basic_consume(self.on_request, queue='rpc_queue')
+        self.channel.basic_consume(self.on_new_slave, queue='rpc_queue')
 
         #for existing slaves
         self.channel.exchange_declare(exchange='logs', type='fanout')
 
-
-    def get_ip_addr(self):
-        """
-        Return IP Address
-        """
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8",80))
-        ip_addr = s.getsockname()[0]
-        s.close()
-        return ip_addr
-    
     def get_vxlan_ip(self):
+        """
+        Returns a unique VXLAN IP address
+        e.g. 192.168.200.1
+        Note: This will only work for upto 255 hosts
+        """
         self.octet_val += 1
         return "192.168.200.%s" % self.octet_val
     
-    def on_request(self, ch, method, props, body):
-        client_ip_addr = body
-        print " [.] client IP Address: %s"  % client_ip_addr
-        #TODO: check if ip address is unique
-        self.slaves.append( (client_ip_addr, self.get_vxlan_ip()) )
+    def on_new_slave(self, ch, method, props, body):
+        """
+        Response when new slave contacts masters
+        Record that slave
+        and broadcast info to existing slaves
+        """
+        slave_ip = body
+        print " [.] slave IP Address: %s"  % client_ip_addr
+        if slave_ip not in slaves:
+            slaves[slave_ip] = self.get_vxlan_ip()
+        new_slave = (slave_ip, slaves[slave_ip])
+        #Response to new slave
         response = cPickle.dumps(self.slaves, cPickle.HIGHEST_PROTOCOL)
-        if True: #len(slaves) == 2: 
-            ch.basic_publish(exchange='',
-                             routing_key=props.reply_to,
-                             properties=pika.BasicProperties(correlation_id = props.correlation_id),
-                             body=response) 
-            ch.basic_ack(delivery_tag = method.delivery_tag)
+        ch.basic_publish(exchange='',
+                         routing_key=props.reply_to,
+                         properties=pika.BasicProperties(correlation_id = props.correlation_id),
+                         body=response) 
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+        
+        #Broadcast new slave    
+        print " [x] Sent %r" % new_slave 
+        self.broadcast(message=
+            cPickle.dumps(new_slave, cPickle.HIGHEST_PROTOCOL))
 
-    def broadcast(self, message="Hello World"):
+    def broadcast(self, message):
         """
         broadcast to existing slaves
         """
-        print " [x] Sent %r" % (message,)
         self.channel.basic_publish(exchange='logs',
                               routing_key='',
                               body=message)
 
-
-    def start_consuming(self):
+    def start_listening(self):
         """
         Start listening for new slaves
         """
-        print " [x] Awaiting RPC requests"
+        print " [x] Awaiting connections from slaves"
         self.channel.start_consuming()
-
-    def run(self):
-        self.start_consuming()
 
     def cleanup(self):
         self.connection.close()
 
 if __name__ == "__main__":
-    master = VinoMasterL()
-    #master.start_consuming()
-    master.start()
+    master = VinoMaster()
+    master.start_listening()
