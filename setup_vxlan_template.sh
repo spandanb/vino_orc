e87bb4fd39393bdbddea2cf40b441657970ad6c3
spandanb <spandan@savinetwork.ca>
2015-05-31 10:31:03 -0400

    Changed setup template file

--- setup_vxlan_template.sh
+++ setup_vxlan_template.sh
@@ -1,17 +1,28 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 #Create VXLAN Tunnel between 2 hosts
 #This is a template file
 #$VXLAN_IP is the assigned ip address of this host
 #$REMOTE_IP is the internal (non-vxlan ip of the remote host)
+#$CONTR_ADDR is the <ip_addr>:<port> of the controller
+#$INTERNAL_IP is the vxlan ip of this bridge
 
 #Create bridge named br0
 sudo ovs-vsctl add-br br0
 
+#Set and configure controller for br0
+sudo ovs-vsctl set-controller br0 tcp:$CONTR_ADDR
+ssudo ovs-vsct set-fail-mode br0 secure
+sudo ovs-vsctl set controller br0 connection-mode=out-of-band
+
+#Add internal port
 sudo ovs-vsctl add-port br0 p0 -- set interface p0 type=internal
 
 mac_addr=`sudo ovs-vsctl get interface p0 mac_in_use`
 sudo ovs-vsctl set interface p0 mac=$$mac_addr
-sudo ifconfig p0 $VXLAN_IP up
+sudo ifconfig p0 $VXLAN_IP/24 up
 
-sudo ovs-vsctl add-port br0 vxlan0 -- set interface vxlan0 type=vxlan options:remote_ip=$REMOTE_IP options:keys=01
+#Uses a fixed vni, allows all VMs to talk to all others, can have 
+# finer control
+#Do the following for all remote hosts
+#sudo ovs-vsctl add-port br0 vxlan0 -- set interface vxlan0 type=vxlan options:remote_ip=$REMOTE_IP options:keys=01
