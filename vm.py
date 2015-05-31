a2c12f04fdee8bcf3e86ea702f0eb855efc93079
spandanb <spandan@savinetwork.ca>
2015-05-31 12:14:16 +0000

    bidirectional v2 working

--- vm.py
+++ vm.py
@@ -1,12 +1,12 @@
 import pika
 
 
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host='localhost'))
+channel = connection.channel()
+
 #Master receiver
 def receive():    
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host='localhost'))
-    channel = connection.channel()
-    
     channel.queue_declare(queue='hello')
     
     print ' [*] Waiting for messages. To exit press CTRL+C'
@@ -22,10 +22,6 @@ def receive():
 
 #Master broadcast
 def bcast():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host='localhost'))
-    channel = connection.channel()
-    
     channel.exchange_declare(exchange='logs',
                              type='fanout')
     
@@ -36,4 +32,28 @@ def bcast():
     print " [x] Sent %r" % (message,)
     connection.close()
 
-bcast()
+
+def fuse():
+    channel.queue_declare(queue='hello') #For receive
+    channel.exchange_declare(exchange='bcast', #For bcast
+                             type='fanout')
+    
+    print ' [*] Waiting for slaves. To exit press CTRL+C'
+
+    def callback(ch, method, properties, body):
+        print " [x] Received %r" % (body,)
+        
+        #To send 
+        message = "info: Hello World! from master"
+        channel.basic_publish(exchange='bcast',
+                              routing_key='',
+                              body=message)
+        print " [x] Sent %r" % (message,)
+    
+    channel.basic_consume(callback,
+                          queue='hello',
+                          no_ack=True)
+    
+    channel.start_consuming()
+
+fuse()
