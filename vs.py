c6339fc14cecbed921b0f3b933c0fe0e693e328a
spandanb <spandan.777@gmail.com>
2015-05-31 12:55:42 +0000

    object orient bidirectional v2
    	modified:   vs.py

--- vs.py
+++ vs.py
@@ -1,47 +1,15 @@
 import pika
 
-ip_addr = "10.12.1.53"
-port = 5672 
-credentials = pika.PlainCredentials('guest', 'guest')
-parameters=pika.ConnectionParameters(ip_addr, port, '/', credentials)
-connection = pika.BlockingConnection(parameters)
-
-channel = connection.channel()
-
-#Slave send        
-def send():
-    channel.queue_declare(queue='hello')
-    
-    channel.basic_publish(exchange='',
-                          routing_key='hello',
-                          body='Hello World!')
-    print " [x] Sent 'Hello World!'"
-
-    #connection.close() #Closes connection with master
-
-#slave listen
-def receive():
-    channel.exchange_declare(exchange='logs',
-                             type='fanout')
-    
-    result = channel.queue_declare(exclusive=True)
-    queue_name = result.method.queue
-    
-    channel.queue_bind(exchange='logs',
-                       queue=queue_name)
-    
-    print ' [*] Waiting for logs. To exit press CTRL+C'
-    
-    def callback(ch, method, properties, body):
-        print " [x] %r" % (body,)
-    
-    channel.basic_consume(callback,
-                          queue=queue_name,
-                          no_ack=True)
-    
-    channel.start_consuming()
 
 def fuse():
+    ip_addr = "10.12.1.53" #Master
+    port = 5672 
+    credentials = pika.PlainCredentials('guest', 'guest')
+    parameters=pika.ConnectionParameters(ip_addr, port, '/', credentials)
+    connection = pika.BlockingConnection(parameters)
+    
+    channel = connection.channel()
+
     #configire for receive
     channel.queue_declare(queue='hello')
 
@@ -54,22 +22,75 @@ def fuse():
     channel.queue_bind(exchange='bcast',
                        queue=queue_name)
     
-    print ' [*] Waiting for slaves. To exit press CTRL+C'
-    
     #callback for something received 
     def callback(ch, method, properties, body):
         print " [x] %r" % (body,)
    
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body='Hello World!')
+    print " [x] Sent 'Hello World!'"
+    
     #This configures consume function, but doesn't 
     # start consuming
     channel.basic_consume(callback,
                           queue=queue_name,
                           no_ack=True)
 
-    channel.basic_publish(exchange='',
-                          routing_key='hello',
-                          body='Hello World!')
-    print " [x] Sent 'Hello World!'"
     channel.start_consuming()
 
-fuse()
+class VinoSlave(object):
+    def __init__(self):
+        ip_addr = "10.12.1.53" #Master
+        port = 5672 
+        credentials = pika.PlainCredentials('guest', 'guest')
+        parameters=pika.ConnectionParameters(ip_addr, port, '/', credentials)
+        connection = pika.BlockingConnection(parameters)
+        
+        self.channel = connection.channel()
+    
+        #configire for receive
+        self.channel.queue_declare(queue='hello')
+    
+        #configure for send
+        self.channel.exchange_declare(exchange='bcast', type='fanout')
+    
+        result = self.channel.queue_declare(exclusive=True)
+        queue_name = result.method.queue
+        
+        self.channel.queue_bind(exchange='bcast',
+                           queue=queue_name)
+        
+        #This configures consume function 
+        self.channel.basic_consume(self.callback,
+                              queue=queue_name,
+                              no_ack=True)
+        self.slaves = {}
+
+    def callback(self, ch, method, properties, body):
+        """
+        callback for something received 
+        """
+        print " [x] %r" % (body,)
+    
+    def hello(self):
+        """
+        Send initial message
+        """
+        message = 'Hello Master'
+        self.channel.basic_publish(exchange='',
+                              routing_key='hello',
+                              body=message)
+        print " [x] Sent %s" % message 
+   
+    def listen(self):
+        """
+        Listen for new slaves
+        """
+        self.channel.start_consuming()
+    
+if __name__ == "__main__":
+	#fuse()
+    slave = VinoSlave()
+    slave.hello()
+    slave.listen()
